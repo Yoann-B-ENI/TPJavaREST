9021de24f6df86a9aa075dc387068b477dbf3c73
Yoann-B-ENI <yoann.battu2024@campus-eni.fr>
2025-04-30 14:44:27 +0200

    S1004: BO Client

---
+++ src/main/java/fr/eni/vioyo/RestLudotheque/bo/Client.java
@@ -0,0 +1,37 @@
+package fr.eni.vioyo.RestLudotheque.bo;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+//@Log
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    private int id;
+
+    @NonNull
+    @Column(nullable = false)
+    private String name;
+
+    @NonNull
+    @Column(nullable = false)
+    private String firstName;
+
+    @NonNull
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @NonNull
+    @Column(nullable = true, unique = true)
+    private String phoneNumber;
+
+    @OneToOne(optional = false, cascade = {CascadeType.ALL}, fetch = FetchType.EAGER)
+    @JoinColumn(name="address_id", unique=true, nullable=false, updatable=false)
+    private Address address;
+}
