1a4e095ee5f8e8718732471b271d122a6011dc43
Yoann-B-ENI <yoann.battu2024@campus-eni.fr>
2025-04-30 15:25:41 +0200

    S1009: create BO Invoice

---
+++ src/main/java/fr/eni/vioyo/RestLudotheque/bo/Invoice.java
@@ -0,0 +1,34 @@
+package fr.eni.vioyo.RestLudotheque.bo;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+//@Log
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+public class Invoice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    private int id;
+
+    @Column(nullable = true)
+    private Date paidDate;
+
+    @NonNull
+    @Column(nullable = false)
+    private Float amount;
+
+    @NonNull
+    @OneToMany()
+    @JoinColumn(name = "inv_rental_id")
+    private List<Rental> rentals;
+}
