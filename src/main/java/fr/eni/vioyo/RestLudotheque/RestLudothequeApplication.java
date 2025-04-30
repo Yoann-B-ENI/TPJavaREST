977a19bcac3be6cdf1dfd413edba3fdb5ca67225
Yoann-B-ENI <yoann.battu2024@campus-eni.fr>
2025-04-30 12:05:04 +0200

    init project

---
+++ src/main/java/fr/eni/vioyo/RestLudotheque/RestLudothequeApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.vioyo.RestLudotheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestLudothequeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestLudothequeApplication.class, args);
+	}
+
+}
