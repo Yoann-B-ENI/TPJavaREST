3d39b427a710187f1f016c2b193c2d9a1d3d1b5f
Yoann-B-ENI <yoann.battu2024@campus-eni.fr>
2025-04-30 14:44:20 +0200

    S1004: BO Address

---
+++ src/main/java/fr/eni/vioyo/RestLudotheque/bo/Address.java
@@ -0,0 +1,31 @@
+package fr.eni.vioyo.RestLudotheque.bo;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+//@Log
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    private int id;
+
+    @NonNull
+    @Column(nullable = false)
+    private String road;
+
+    @NonNull
+    @Column(nullable = false)
+    private String zipCode;
+
+    @NonNull
+    @Column(nullable = false)
+    private String town;
+}
