662a3f1c393434fcbadad61f35aa20febf9c5a38
Yoann-B-ENI <yoann.battu2024@campus-eni.fr>
2025-05-02 15:36:14 +0200

    S2011: exception and test

--- src/main/java/fr/eni/vioyo/RestLudotheque/bll/AddressService.java
+++ src/main/java/fr/eni/vioyo/RestLudotheque/bll/AddressService.java
@@ -1,4 +1,10 @@
 package fr.eni.vioyo.RestLudotheque.bll;
 
+import fr.eni.vioyo.RestLudotheque.bo.Address;
+import fr.eni.vioyo.RestLudotheque.exceptions.BONotInDBException;
+import fr.eni.vioyo.RestLudotheque.exceptions.NoIDException;
+import fr.eni.vioyo.RestLudotheque.exceptions.NullBOException;
+
 public interface AddressService {
+    Address updateAddress(Address adr) throws NoIDException, NullBOException, BONotInDBException;
 }
--- src/main/java/fr/eni/vioyo/RestLudotheque/bll/AddressServiceImpl.java
+++ src/main/java/fr/eni/vioyo/RestLudotheque/bll/AddressServiceImpl.java
@@ -1,7 +1,11 @@
 package fr.eni.vioyo.RestLudotheque.bll;
 
 import fr.eni.vioyo.RestLudotheque.bo.Address;
+import fr.eni.vioyo.RestLudotheque.bo.Client;
 import fr.eni.vioyo.RestLudotheque.dal.AddressRepository;
+import fr.eni.vioyo.RestLudotheque.exceptions.BONotInDBException;
+import fr.eni.vioyo.RestLudotheque.exceptions.NoIDException;
+import fr.eni.vioyo.RestLudotheque.exceptions.NullBOException;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.OptimisticLockingFailureException;
@@ -9,24 +13,42 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Log
-public class AddressServiceImpl {
+public class AddressServiceImpl implements AddressService{
 
     @Autowired
     private AddressRepository repo;
 
-    public void updateAddress(Address adr){
+    @Override
+    public Address updateAddress(Address adr) throws NoIDException, NullBOException, BONotInDBException {
+        if (adr == null){
+            NullBOException excp = new NullBOException(Address.class);
+            log.severe(excp.getMessage());
+            throw excp;
+        }
+        if (adr.getId() <= 0){
+            NoIDException excp = new NoIDException(adr);
+            log.severe(excp.getMessage());
+            throw excp;
+        }
+
         try {
-            this.repo.save(adr);
+            // # # # #
+            adr = this.repo.save(adr);
+            // # # # #
         } catch (IllegalArgumentException e) {
-            log.severe("Address " + adr + " is null");
-            throw new RuntimeException("Address " + adr + " is null");
+            NullBOException excp = new NullBOException(Address.class);
+            log.severe(excp.getMessage());
+            throw excp;
         } catch (OptimisticLockingFailureException e) {
-            log.severe("Address " + adr + " didn't exist in DB");
-            throw new RuntimeException("Address " + adr + " didn't exist in DB");
+            BONotInDBException excp = new BONotInDBException(adr);
+            log.severe(excp.getMessage());
+            throw excp;
         } catch (Exception e) {
             log.severe(e.getMessage());
             throw new RuntimeException(e);
         }
+
+        return adr;
     }
 
 }
